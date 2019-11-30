f044eb998fbf1aa69fe728463917930391887ef1
Nieves Montes <mnm24896@gmail.com>
2019-11-30 16:38:05 +0100

    process data finished

--- process_data.py
+++ process_data.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+"""
+description here...
+"""
 
 #%% Import data from JSON file
 import json
@@ -11,34 +14,48 @@ pipelines = data['features']    # list of dictionaries
 import pandas as pd
 vertices = pd.DataFrame(columns=['lon', 'lat'])
 for pipe in pipelines:
-    if ((pipe['attributes']['PIPE_TYPE']=='Crude Oil') & (pipe['attributes']['PIPE_CLASS']=='Intrastate')):
+    if ((pipe['attributes']['PIPE_TYPE']=='Crude Oil') & 
+        (pipe['attributes']['PIPE_CLASS']=='Intrastate')):
         node_list = pipe['geometry']['paths'][0]
         for node in node_list:
             entry = pd.DataFrame({'lon': node[0], 'lat': node[1]}, index=[0])
             vertices = vertices.append(entry, ignore_index=True)        
-vertices = vertices.drop_duplicates(keep='first', inplace=False)
+vertices.drop_duplicates(keep='first', inplace=True)
+vertices.reset_index(inplace=True)
+vertices.drop(columns=['index'], inplace=True)
 #%%
 
 #%% Build a dataframe of directed edges
 edges = pd.DataFrame(columns=['id1', 'id2', 'size'])
 for pipe in pipelines:
-    if ((pipe['attributes']['PIPE_TYPE']=='Crude Oil') & (pipe['attributes']['PIPE_CLASS']=='Intrastate')):
+    if ((pipe['attributes']['PIPE_TYPE']=='Crude Oil') & 
+        (pipe['attributes']['PIPE_CLASS']=='Intrastate')):
         node_list = pipe['geometry']['paths'][0]
         for i in range(len(node_list)-1):
             cord_node1 = node_list[i]
             cord_node2 = node_list[i+1]
-            id1 = vertices.loc[(vertices['lon']==cord_node1[0]) & (vertices['lat']==cord_node1[1])].index[0]
-            id2 = vertices.loc[(vertices['lon']==cord_node2[0]) & (vertices['lat']==cord_node2[1])].index[0]
+            id1 = vertices.loc[(vertices['lon']==cord_node1[0]) & 
+                               (vertices['lat']==cord_node1[1])].index[0]
+            id2 = vertices.loc[(vertices['lon']==cord_node2[0]) & 
+                               (vertices['lat']==cord_node2[1])].index[0]
             size = int(pipe['attributes']['PIPE_SIZE'])
-            entry = pd.DataFrame({'id1':id1, 'id2':id2, 'size':size}, index=[0])
+            entry = pd.DataFrame({'id1':id1, 'id2':id2, 'size':size}, 
+                                 index=[0])
             edges = edges.append(entry, ignore_index=True)
 edges = edges.astype(int)
-edges = edges.drop_duplicates(keep='first', inplace=False)
+edges.drop_duplicates(keep='first', inplace=True)
+edges.reset_index(inplace=True)
+edges.drop(columns=['index'], inplace=True)
+edges['capacity'] = edges['size']**2
 #%%
+"""
+Flow through a pipeline is proportional to its transversal area. Therefore,
+the capacity of an edge is computed as its size squared.
+"""
 
 #%% Build list of segments
 lines = []
-sizes = []
+capacities = []
 for index, row in edges.iterrows():
     id1 = row['id1']
     lon1 = vertices.loc[id1,:]['lon']
@@ -49,14 +66,14 @@ for index, row in edges.iterrows():
     lat2 = vertices.loc[id2,:]['lat']
     cord2 = (lon2, lat2)
     lines.append([cord1, cord2])
-    sizes.append(row['size'])
+    capacities.append(row['capacity'])
 #%%
     
 #%% Translate sizes into colors
-colors_dict = {2:'#e41a1c', 3:'#377eb8', 4:'#4daf4a'}
+colors_dict = {4:'#e41a1c', 9:'#377eb8', 16:'#4daf4a'}
 colors = []
-for size in sizes:
-    colors.append(colors_dict[size])
+for cap in capacities:
+    colors.append(colors_dict[cap])
 #%%
 
 #%% Plot the graph
@@ -72,14 +89,13 @@ plt.xlabel('Longitude')
 plt.ylabel('Latitude')
 line_segments = LineCollection(lines, colors=colors, linewidths=4)
 ax.add_collection(line_segments)
-custom_lines = [Line2D([0], [0], color=colors_dict.get(2), lw=3),
-                Line2D([0], [0], color=colors_dict.get(3), lw=4),
-                Line2D([0], [0], color=colors_dict.get(4), lw=4)]
-ax.legend(custom_lines, [2, 3, 4], title='Pipe size:')
+custom_lines = [Line2D([0], [0], color=colors_dict.get(4), lw=3),
+                Line2D([0], [0], color=colors_dict.get(9), lw=4),
+                Line2D([0], [0], color=colors_dict.get(16), lw=4)]
+ax.legend(custom_lines, [4, 9, 16], title='Capacity:')
 plt.scatter(x=vertices['lon'], y=vertices['lat'], s=50, color='black')
 plt.grid()
 plt.savefig('graph.png')
-plt.show()
 #%%
 
 #%% Export important data to files
